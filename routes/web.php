71657f6979de10e8661f22146292fe5268d83448
ulfamuasifa <ulfamuasifa@gmail.com>
2024-02-29 15:09:20 +0700

    rpl2_galery_ulfa

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\GaleryController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('login');
+});
+Route::middleware(['auth'])->group(function(){
+    Route::resource('galery',GaleryController::class);
+ 
+   
+});
+Route::get('/login',[AuthController::class,'index'])->name('login');
+Route::post('/login',[AuthController::class,'postLogin'])->name('postlogin');
+Route::get('/register',[AuthController::class,'register'])->name('register');
+Route::post('/register',[AuthController::class,'postRegister'])->name('postregister');
+Route::get('/logout',[AuthController::class,'logout'])->name('logout');
+
